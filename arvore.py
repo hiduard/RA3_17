1308e314de3f3aed6b7824689519600ad0e299a6
Gabriel Barbosa Fernandes de Oliveira <gabriel521ton@gmail.com>
2026-05-29 20:24:12 -0300

    fix: corrige nome do grupo de RA2 para RA3

--- arvore.py
+++ arvore.py
@@ -2,7 +2,7 @@
 # Eduardo Hideo Itinoseke Ogassawara - hiduard
 # Gabriel Barbosa Fernandes de Oliveira - GabrielBarbosaFernandes
 #
-# Nome do grupo no Canvas: RA2 17
+# Nome do grupo no Canvas: RA3 17
 
 import json
 import os
